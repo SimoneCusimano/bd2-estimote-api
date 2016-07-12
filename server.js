2723ea324e3b900c6fda9751b0d8dccf98911db1
lodovica <lodo.marchesi@gmail.com>
2016-07-13 00:13:13 +0200

    Edited test call route url

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ router.use(function(req, res, next) {
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });
+router.get('/test', function(req, res) {
+    res.json({ message: 'API up & running!' });
 });
 
 // more routes for our API will happen here
